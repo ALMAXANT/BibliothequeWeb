44bfffd72ded5fc139cdb573bab27c53bebcf67f
ALMAXANT <81476289+ALMAXANT@users.noreply.github.com>
2021-06-10 23:05:22 +0200

    Delete Librairie directory

--- Librairie/client/calendrier.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-<input id="date" type="date" value="2017-06-01">
-
-</html>
--- Librairie/contact.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.html'; ?>
-
-<?php include 'formulaire-contact.php'; ?>
-
-<?php include 'footer.html'; ?>
-        
